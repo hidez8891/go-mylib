fadc0e93bd080f8542a9edf5ef486b4e61151f8c
hidez8891 <427357+hidez8891@users.noreply.github.com>
2022-05-06 17:27:45 +0900

    zip internal headers and records

---
+++ zip/struct.go
@@ -0,0 +1,395 @@
+package zip
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"time"
+
+	"go-mylib/byteio"
+)
+
+const (
+	signLocalFileHeader        string = "PK\x03\x04"
+	signCentralDirectoryHeader string = "PK\x01\x02"
+	signDataDescriptor         string = "PK\x07\x08"
+	signEndCentralDirectory    string = "PK\x05\x06"
+
+	sizeLocalFileHeader        int = 30
+	sizeCentralDirectoryHeader int = 46
+	sizeDataDescriptor         int = 16
+	sizeEndCentralDirectory    int = 22
+)
+
+const (
+	FlagCompressed     uint16 = 0x0001
+	FlagDataDescriptor uint16 = 0x0008
+	FlagUTF8           uint16 = 0x0800
+)
+
+const (
+	MethodStored   uint16 = 0
+	MethodDeflated uint16 = 8
+)
+
+type localFileHeader struct {
+	RequireVersion   uint16
+	Flags            uint16
+	Method           uint16
+	ModifiedTime     time.Time
+	CRC32            uint32
+	CompressedSize   uint32
+	UncompressedSize uint32
+	FileName         string
+	ExtraFields      []byte
+}
+
+func (h *localFileHeader) ReadFrom(r io.Reader) (int64, error) {
+	var sign [4]byte
+	if _, err := r.Read(sign[:]); err != nil {
+		return 0, err
+	}
+	if string(sign[:]) != signLocalFileHeader {
+		return 0, errors.New("invalid zip format: not found local file header signature")
+	}
+
+	var data [sizeLocalFileHeader - 4]byte
+	if _, err := io.ReadAtLeast(r, data[:], len(data)); err != nil {
+		return 0, err
+	}
+
+	rr := bytes.NewReader(data[:])
+	byteio.GetUint16LE(rr, &h.RequireVersion)
+	byteio.GetUint16LE(rr, &h.Flags)
+	byteio.GetUint16LE(rr, &h.Method)
+	var modtime, moddate uint16
+	byteio.GetUint16LE(rr, &modtime)
+	byteio.GetUint16LE(rr, &moddate)
+	h.ModifiedTime = uint32ToUTCTime(moddate, modtime)
+	byteio.GetUint32LE(rr, &h.CRC32)
+	byteio.GetUint32LE(rr, &h.CompressedSize)
+	byteio.GetUint32LE(rr, &h.UncompressedSize)
+	var nameSize, extraSize uint16
+	byteio.GetUint16LE(rr, &nameSize)
+	byteio.GetUint16LE(rr, &extraSize)
+
+	if nameSize == 0 {
+		return 0, errors.New("invalid file name: name length is 0")
+	}
+	nameBuf := make([]byte, nameSize)
+	if _, err := io.ReadAtLeast(r, nameBuf, len(nameBuf)); err != nil {
+		return 0, err
+	}
+	h.FileName = string(nameBuf)
+
+	if extraSize != 0 {
+		extraBuf := make([]byte, extraSize)
+		if _, err := io.ReadAtLeast(r, extraBuf, len(extraBuf)); err != nil {
+			return 0, err
+		}
+		h.ExtraFields = extraBuf
+	}
+
+	return int64(sizeLocalFileHeader) + int64(nameSize) + int64(extraSize), nil
+}
+
+func (h *localFileHeader) WriteTo(w io.Writer) (int64, error) {
+	buf := new(bytes.Buffer)
+	buf.Write([]byte(signLocalFileHeader))
+	byteio.WriteUint16LE(buf, h.RequireVersion)
+	byteio.WriteUint16LE(buf, h.Flags)
+	byteio.WriteUint16LE(buf, h.Method)
+	moddate, modtime := utcTimeToUint32(h.ModifiedTime)
+	byteio.WriteUint16LE(buf, modtime)
+	byteio.WriteUint16LE(buf, moddate)
+	byteio.WriteUint32LE(buf, h.CRC32)
+	byteio.WriteUint32LE(buf, h.CompressedSize)
+	byteio.WriteUint32LE(buf, h.UncompressedSize)
+	byteio.WriteUint16LE(buf, uint16(len(h.FileName)))
+	byteio.WriteUint16LE(buf, uint16(len(h.ExtraFields)))
+
+	if len(h.FileName) == 0 {
+		return 0, errors.New("invalid file name: name length is 0")
+	}
+	if _, err := buf.Write([]byte(h.FileName)); err != nil {
+		return 0, err
+	}
+
+	if len(h.ExtraFields) != 0 {
+		if _, err := buf.Write([]byte(h.ExtraFields)); err != nil {
+			return 0, err
+		}
+	}
+
+	n, err := w.Write(buf.Bytes())
+	return int64(n), err
+}
+
+type centralDirectoryHeader struct {
+	localFileHeader
+	GenerateVersion   uint16
+	InternalFileAttr  uint16
+	ExternalFileAttr  uint32
+	LocalHeaderOffset uint32
+	Comment           string
+}
+
+func (h *centralDirectoryHeader) ReadFrom(r io.Reader) (int64, error) {
+	var sign [4]byte
+	if _, err := r.Read(sign[:]); err != nil {
+		return 0, err
+	}
+	if string(sign[:]) != signCentralDirectoryHeader {
+		return 0, errors.New("invalid zip format: not found central directory header signature")
+	}
+
+	var data [sizeCentralDirectoryHeader - 4]byte
+	if _, err := io.ReadAtLeast(r, data[:], len(data)); err != nil {
+		return 0, err
+	}
+
+	rr := bytes.NewReader(data[:])
+	byteio.GetUint16LE(rr, &h.GenerateVersion)
+	byteio.GetUint16LE(rr, &h.RequireVersion)
+	byteio.GetUint16LE(rr, &h.Flags)
+	byteio.GetUint16LE(rr, &h.Method)
+	var modtime, moddate uint16
+	byteio.GetUint16LE(rr, &modtime)
+	byteio.GetUint16LE(rr, &moddate)
+	h.ModifiedTime = uint32ToUTCTime(moddate, modtime)
+	byteio.GetUint32LE(rr, &h.CRC32)
+	byteio.GetUint32LE(rr, &h.CompressedSize)
+	byteio.GetUint32LE(rr, &h.UncompressedSize)
+	var nameSize, extraSize, commentSize uint16
+	byteio.GetUint16LE(rr, &nameSize)
+	byteio.GetUint16LE(rr, &extraSize)
+	byteio.GetUint16LE(rr, &commentSize)
+	var diskNumber uint16
+	byteio.GetUint16LE(rr, &diskNumber)
+	byteio.GetUint16LE(rr, &h.InternalFileAttr)
+	byteio.GetUint32LE(rr, &h.ExternalFileAttr)
+	byteio.GetUint32LE(rr, &h.LocalHeaderOffset)
+
+	if diskNumber != 0 {
+		return 0, errors.New("unsupport split zip file")
+	}
+
+	if nameSize == 0 {
+		return 0, errors.New("invalid file name: name length is 0")
+	}
+	nameBuf := make([]byte, nameSize)
+	if _, err := io.ReadAtLeast(r, nameBuf, len(nameBuf)); err != nil {
+		return 0, err
+	}
+	h.FileName = string(nameBuf)
+
+	if extraSize != 0 {
+		extraBuf := make([]byte, extraSize)
+		if _, err := io.ReadAtLeast(r, extraBuf, len(extraBuf)); err != nil {
+			return 0, err
+		}
+		h.ExtraFields = extraBuf
+	}
+
+	if commentSize != 0 {
+		commentBuf := make([]byte, commentSize)
+		if _, err := io.ReadAtLeast(r, commentBuf, len(commentBuf)); err != nil {
+			return 0, err
+		}
+		h.Comment = string(commentBuf)
+	}
+
+	size := sizeLocalFileHeader
+	size += int(nameSize) + int(extraSize) + int(commentSize)
+	return int64(size), nil
+}
+
+func (h *centralDirectoryHeader) WriteTo(w io.Writer) (int64, error) {
+	buf := new(bytes.Buffer)
+	w.Write([]byte(signCentralDirectoryHeader))
+	byteio.WriteUint16LE(buf, h.GenerateVersion)
+	byteio.WriteUint16LE(buf, h.RequireVersion)
+	byteio.WriteUint16LE(buf, h.Flags)
+	byteio.WriteUint16LE(buf, h.Method)
+	moddate, modtime := utcTimeToUint32(h.ModifiedTime)
+	byteio.WriteUint16LE(buf, modtime)
+	byteio.WriteUint16LE(buf, moddate)
+	byteio.WriteUint32LE(buf, h.CRC32)
+	byteio.WriteUint32LE(buf, h.CompressedSize)
+	byteio.WriteUint32LE(buf, h.UncompressedSize)
+	byteio.WriteUint16LE(buf, uint16(len(h.FileName)))
+	byteio.WriteUint16LE(buf, uint16(len(h.ExtraFields)))
+	byteio.WriteUint16LE(buf, uint16(len(h.Comment)))
+	byteio.WriteUint16LE(buf, 0)
+	byteio.WriteUint16LE(buf, h.InternalFileAttr)
+	byteio.WriteUint32LE(buf, h.ExternalFileAttr)
+	byteio.WriteUint32LE(buf, h.LocalHeaderOffset)
+
+	if len(h.FileName) == 0 {
+		return 0, errors.New("invalid file name: name length is 0")
+	}
+	if _, err := buf.Write([]byte(h.FileName)); err != nil {
+		return 0, err
+	}
+
+	if len(h.ExtraFields) != 0 {
+		if _, err := buf.Write(h.ExtraFields); err != nil {
+			return 0, err
+		}
+	}
+
+	if len(h.Comment) != 0 {
+		if _, err := buf.Write([]byte(h.Comment)); err != nil {
+			return 0, err
+		}
+	}
+
+	n, err := w.Write(buf.Bytes())
+	return int64(n), err
+}
+
+type dataDescriptor struct {
+	CRC32            uint32
+	CompressedSize   uint32
+	UncompressedSize uint32
+}
+
+func (d *dataDescriptor) ReadFrom(r io.Reader) (int64, error) {
+	var buf [sizeDataDescriptor]byte
+	size := len(buf) - 4
+	if _, err := io.ReadAtLeast(r, buf[:size], size); err != nil {
+		return 0, err
+	}
+
+	var sign [4]byte
+	rr := bytes.NewReader(buf[:4])
+	if _, err := rr.Read(sign[:]); err != nil {
+		return 0, err
+	}
+	if string(sign[:]) == signDataDescriptor {
+		// load additional data
+		size += 4
+		if _, err := io.ReadAtLeast(r, buf[size-4:], 4); err != nil {
+			return 0, err
+		}
+		rr = bytes.NewReader(buf[4:])
+	} else {
+		rr = bytes.NewReader(buf[:size])
+	}
+
+	byteio.GetUint32LE(rr, &d.CRC32)
+	byteio.GetUint32LE(rr, &d.CompressedSize)
+	byteio.GetUint32LE(rr, &d.UncompressedSize)
+
+	return int64(size), nil
+}
+
+func (d *dataDescriptor) WriteTo(w io.Writer) (int64, error) {
+	buf := new(bytes.Buffer)
+	buf.Write([]byte(signDataDescriptor))
+	byteio.WriteUint32LE(buf, d.CRC32)
+	byteio.WriteUint32LE(buf, d.CompressedSize)
+	byteio.WriteUint32LE(buf, d.UncompressedSize)
+
+	if _, err := w.Write(buf.Bytes()); err != nil {
+		return 0, err
+	}
+	return int64(buf.Len()), nil
+}
+
+type endCentralDirectory struct {
+	numberOfEntries          uint16
+	sizeOfCentralDirectories uint32
+	offsetCentralDirectory   uint32
+	Comment                  string
+}
+
+func (e *endCentralDirectory) ReadFrom(r io.Reader) (int64, error) {
+	var sign [4]byte
+	if _, err := r.Read(sign[:]); err != nil {
+		return 0, err
+	}
+	if string(sign[:]) != signEndCentralDirectory {
+		return 0, errors.New("invalid zip format: not found end of central directory signature")
+	}
+
+	var buf [sizeEndCentralDirectory - 4]byte
+	if _, err := r.Read(buf[:]); err != nil {
+		return 0, err
+	}
+	rr := bytes.NewReader(buf[:])
+	var (
+		numThisDisk      uint16
+		numStartDirDisk  uint16
+		numEntriesInDisk uint16
+		commentSize      uint16
+	)
+	byteio.GetUint16LE(rr, &numThisDisk)
+	byteio.GetUint16LE(rr, &numStartDirDisk)
+	byteio.GetUint16LE(rr, &numEntriesInDisk)
+	byteio.GetUint16LE(rr, &e.numberOfEntries)
+	byteio.GetUint32LE(rr, &e.sizeOfCentralDirectories)
+	byteio.GetUint32LE(rr, &e.offsetCentralDirectory)
+	byteio.GetUint16LE(rr, &commentSize)
+
+	if numThisDisk != 0 || numStartDirDisk != 0 || numEntriesInDisk != e.numberOfEntries {
+		return 0, errors.New("unsupport split zip file")
+	}
+
+	if commentSize != 0 {
+		commentBuf := make([]byte, commentSize)
+		if _, err := r.Read(commentBuf); err != nil {
+			return 0, err
+		}
+		e.Comment = string(commentBuf)
+	}
+
+	size := int(sizeEndCentralDirectory) + int(commentSize)
+	return int64(size), nil
+}
+
+func (e *endCentralDirectory) WriteTo(w io.Writer) (int64, error) {
+	buf := new(bytes.Buffer)
+	w.Write([]byte(signEndCentralDirectory))
+	byteio.WriteUint16LE(buf, 0)
+	byteio.WriteUint16LE(buf, 0)
+	byteio.WriteUint16LE(buf, e.numberOfEntries)
+	byteio.WriteUint16LE(buf, e.numberOfEntries)
+	byteio.WriteUint32LE(buf, e.sizeOfCentralDirectories)
+	byteio.WriteUint32LE(buf, e.offsetCentralDirectory)
+	byteio.WriteUint16LE(buf, uint16(len(e.Comment)))
+
+	if _, err := buf.Write([]byte(e.Comment)); err != nil {
+		return 0, err
+	}
+
+	if _, err := w.Write(buf.Bytes()); err != nil {
+		return 0, err
+	}
+	return int64(buf.Len()), nil
+}
+
+func uint32ToUTCTime(dates uint16, times uint16) time.Time {
+	year := int((dates >> 9) & 0x7f)
+	monh := int((dates >> 5) & 0x0f)
+	days := int((dates >> 0) & 0x1f)
+	hour := int((times >> 11) & 0x1f)
+	mins := int((times >> 05) & 0x3f)
+	secs := int((times >> 00) & 0x1f)
+
+	year += 1980
+	secs *= 2
+
+	return time.Date(year, time.Month(monh), days, hour, mins, secs, 0, time.UTC)
+}
+
+func utcTimeToUint32(t time.Time) (dates uint16, times uint16) {
+	t = t.UTC()
+	dates |= (uint16(t.Year()) - 1980) << 9
+	dates |= uint16(t.Month()) << 5
+	dates |= uint16(t.Day())
+	times |= uint16(t.Hour()) << 11
+	times |= uint16(t.Minute()) << 5
+	times |= uint16(t.Second()) / 2
+	return dates, times
+}
---
+++ zip/struct_test.go
@@ -0,0 +1,361 @@
+package zip
+
+import (
+	"bytes"
+	"testing"
+	"time"
+)
+
+func hexTo(s rune) byte {
+	if rune('0') <= s && s <= rune('9') {
+		return byte(s - rune('0'))
+	}
+	if rune('a') <= s && s <= rune('f') {
+		return byte(s - rune('a') + 10)
+	}
+	if rune('A') <= s && s <= rune('F') {
+		return byte(s - rune('A') + 10)
+	}
+	panic("not hex rune")
+}
+
+func isHex(s rune) bool {
+	if rune('0') <= s && s <= rune('9') {
+		return true
+	}
+	if rune('a') <= s && s <= rune('f') {
+		return true
+	}
+	if rune('A') <= s && s <= rune('F') {
+		return true
+	}
+	return false
+}
+
+func hexToBytes(hex string) []byte {
+	out := make([]byte, 0)
+
+	for i := 0; i < len(hex); {
+		s := rune(hex[i])
+		if !isHex(s) {
+			i += 1
+			continue
+		}
+		b := hexTo(s)
+
+		s = rune(hex[i+1])
+		if !isHex(s) {
+			panic("no hex string")
+		}
+		b = (b << 4) | hexTo(s)
+
+		out = append(out, b)
+		i += 2
+	}
+
+	return out
+}
+
+func Test_localFileHeader(t *testing.T) {
+	src := hexToBytes(`
+		50 4b 03 04 14 00 08 00 08 00 5c 64 a6 54 04 03
+		02 01 78 56 34 12 09 ef cd ab 08 00 04 00 66 69
+		6c 65 6e 61 6d 65 04 03 02 01
+	`)
+	expect := localFileHeader{
+		RequireVersion:   0x0014,
+		Flags:            0x0008,
+		Method:           8,
+		ModifiedTime:     time.Date(2022, time.Month(5), 6, 12, 34, 56, 0, time.UTC),
+		CRC32:            0x01020304,
+		CompressedSize:   0x12345678,
+		UncompressedSize: 0xabcdef09,
+		FileName:         "filename",
+		ExtraFields:      hexToBytes("04 03 02 01"),
+	}
+
+	{
+		r := bytes.NewReader(src)
+
+		h := new(localFileHeader)
+		if _, err := h.ReadFrom(r); err != nil {
+			t.Fatal(err)
+		}
+
+		if h.RequireVersion != expect.RequireVersion {
+			t.Errorf("RequireVersion=%x, want=%x", h.RequireVersion, expect.RequireVersion)
+		}
+		if h.Flags != expect.Flags {
+			t.Errorf("Flags=%x, want=%x", h.Flags, expect.Flags)
+		}
+		if h.Method != expect.Method {
+			t.Errorf("Method=%x, want=%x", h.Method, expect.Method)
+		}
+		if !h.ModifiedTime.Equal(expect.ModifiedTime) {
+			t.Errorf("ModifiedTime=%#v, want=%#v", h.ModifiedTime, expect.ModifiedTime)
+		}
+		if h.CRC32 != expect.CRC32 {
+			t.Errorf("CRC32=%x, want=%x", h.CRC32, expect.CRC32)
+		}
+		if h.CompressedSize != expect.CompressedSize {
+			t.Errorf("CompressedSize=%d, want=%d", h.CompressedSize, expect.CompressedSize)
+		}
+		if h.UncompressedSize != expect.UncompressedSize {
+			t.Errorf("UncompressedSize=%d, want=%d", h.UncompressedSize, expect.UncompressedSize)
+		}
+		if h.FileName != expect.FileName {
+			t.Errorf("FileName=%q, want=%q", h.FileName, expect.FileName)
+		}
+
+		if len(h.ExtraFields) != len(expect.ExtraFields) {
+			t.Fatalf("ExtraFields size=%d, want=%d", len(h.ExtraFields), len(expect.ExtraFields))
+		}
+		for i := range h.ExtraFields {
+			if h.ExtraFields[i] != expect.ExtraFields[i] {
+				t.Fatalf("ExtraFields[%d]=%x, want=%x", i, h.ExtraFields[i], expect.ExtraFields[i])
+			}
+		}
+	}
+
+	{
+		w := new(bytes.Buffer)
+		if _, err := expect.WriteTo(w); err != nil {
+			t.Fatal(err)
+		}
+		dst := w.Bytes()
+
+		if len(dst) != len(src) {
+			t.Fatalf("WriteTo write size=%d, want=%d", len(dst), len(src))
+		}
+		for i := range dst {
+			if dst[i] != src[i] {
+				t.Fatalf("WriteTo result[%d]=%x, want=%x", i, dst[i], src[i])
+			}
+		}
+	}
+}
+
+func Test_centralDirectoryHeader(t *testing.T) {
+	src := hexToBytes(`
+        50 4b 01 02 20 00 14 00 08 00 08 00 5c 64 a6 54
+		04 03 02 01 78 56 34 12 09 ef cd ab 08 00 04 00
+		07 00 00 00 01 00 03 00 00 00 b9 0f 00 00 66 69
+		6c 65 6e 61 6d 65 04 03 02 01 63 6F 6D 6D 65 6E
+		74
+	`)
+	expect := centralDirectoryHeader{
+		localFileHeader: localFileHeader{
+			RequireVersion:   0x0014,
+			Flags:            0x0008,
+			Method:           8,
+			ModifiedTime:     time.Date(2022, time.Month(5), 6, 12, 34, 56, 0, time.UTC),
+			CRC32:            0x01020304,
+			CompressedSize:   0x12345678,
+			UncompressedSize: 0xabcdef09,
+			FileName:         "filename",
+			ExtraFields:      hexToBytes("04 03 02 01"),
+		},
+		GenerateVersion:   0x0020,
+		InternalFileAttr:  0x0001,
+		ExternalFileAttr:  0x0003,
+		LocalHeaderOffset: 0x0fb9,
+		Comment:           "comment",
+	}
+
+	{
+		r := bytes.NewReader(src)
+
+		h := new(centralDirectoryHeader)
+		if _, err := h.ReadFrom(r); err != nil {
+			t.Fatal(err)
+		}
+
+		if h.GenerateVersion != expect.GenerateVersion {
+			t.Errorf("GenerateVersion=%x, want=%x", h.GenerateVersion, expect.GenerateVersion)
+		}
+		if h.RequireVersion != expect.RequireVersion {
+			t.Errorf("RequireVersion=%x, want=%x", h.RequireVersion, expect.RequireVersion)
+		}
+		if h.Flags != expect.Flags {
+			t.Errorf("Flags=%x, want=%x", h.Flags, expect.Flags)
+		}
+		if h.Method != expect.Method {
+			t.Errorf("Method=%x, want=%x", h.Method, expect.Method)
+		}
+		if !h.ModifiedTime.Equal(expect.ModifiedTime) {
+			t.Errorf("ModifiedTime=%#v, want=%#v", h.ModifiedTime, expect.ModifiedTime)
+		}
+		if h.CRC32 != expect.CRC32 {
+			t.Errorf("CRC32=%x, want=%x", h.CRC32, expect.CRC32)
+		}
+		if h.CompressedSize != expect.CompressedSize {
+			t.Errorf("CompressedSize=%d, want=%d", h.CompressedSize, expect.CompressedSize)
+		}
+		if h.UncompressedSize != expect.UncompressedSize {
+			t.Errorf("UncompressedSize=%d, want=%d", h.UncompressedSize, expect.UncompressedSize)
+		}
+		if h.FileName != expect.FileName {
+			t.Errorf("FileName=%q, want=%q", h.FileName, expect.FileName)
+		}
+
+		if len(h.ExtraFields) != len(expect.ExtraFields) {
+			t.Fatalf("ExtraFields size=%d, want=%d", len(h.ExtraFields), len(expect.ExtraFields))
+		}
+		for i := range h.ExtraFields {
+			if h.ExtraFields[i] != expect.ExtraFields[i] {
+				t.Fatalf("ExtraFields[%d]=%x, want=%x", i, h.ExtraFields[i], expect.ExtraFields[i])
+			}
+		}
+
+		if h.Comment != expect.Comment {
+			t.Errorf("Comment=%q, want=%q", h.Comment, expect.Comment)
+		}
+		if h.InternalFileAttr != expect.InternalFileAttr {
+			t.Errorf("InternalFileAttr=%x, want=%x", h.InternalFileAttr, expect.InternalFileAttr)
+		}
+		if h.ExternalFileAttr != expect.ExternalFileAttr {
+			t.Errorf("ExternalFileAttr=%x, want=%x", h.ExternalFileAttr, expect.ExternalFileAttr)
+		}
+		if h.LocalHeaderOffset != expect.LocalHeaderOffset {
+			t.Errorf("LocalHeaderOffset=%x, want=%x", h.LocalHeaderOffset, expect.LocalHeaderOffset)
+		}
+	}
+
+	{
+		w := new(bytes.Buffer)
+		if _, err := expect.WriteTo(w); err != nil {
+			t.Fatal(err)
+		}
+		dst := w.Bytes()
+
+		if len(dst) != len(src) {
+			t.Fatalf("WriteTo write size=%d, want=%d", len(dst), len(src))
+		}
+		for i := range dst {
+			if dst[i] != src[i] {
+				t.Fatalf("WriteTo result[%d]=%x, want=%x", i, dst[i], src[i])
+			}
+		}
+	}
+}
+
+func Test_dataDescriptor(t *testing.T) {
+	tests := []struct {
+		src    []byte
+		expect dataDescriptor
+	}{
+		{
+			src: hexToBytes(`
+				50 4b 07 08 04 03 02 01 4f 3f 2f 1f 8f 7f 6f 5f
+			`),
+			expect: dataDescriptor{
+				CRC32:            0x01020304,
+				CompressedSize:   0x1f2f3f4f,
+				UncompressedSize: 0x5f6f7f8f,
+			},
+		},
+		{
+			src: hexToBytes(`
+				04 03 02 01 4f 3f 2f 1f 8f 7f 6f 5f
+			`),
+			expect: dataDescriptor{
+				CRC32:            0x01020304,
+				CompressedSize:   0x1f2f3f4f,
+				UncompressedSize: 0x5f6f7f8f,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		{
+			r := bytes.NewReader(tt.src)
+			d := new(dataDescriptor)
+
+			if _, err := d.ReadFrom(r); err != nil {
+				t.Fatal(err)
+			}
+			if d.CRC32 != tt.expect.CRC32 {
+				t.Errorf("CRC32=%x, want=%x", d.CRC32, tt.expect.CRC32)
+			}
+			if d.CompressedSize != tt.expect.CompressedSize {
+				t.Errorf("CompressedSize=%d, want=%d", d.CompressedSize, tt.expect.CompressedSize)
+			}
+			if d.UncompressedSize != tt.expect.UncompressedSize {
+				t.Errorf("UncompressedSize=%d, want=%d", d.UncompressedSize, tt.expect.UncompressedSize)
+			}
+		}
+
+		if len(tt.src) != sizeDataDescriptor {
+			continue // WriteTo test skip
+		}
+
+		{
+			w := new(bytes.Buffer)
+			if _, err := tt.expect.WriteTo(w); err != nil {
+				t.Fatal(err)
+			}
+			dst := w.Bytes()
+
+			if len(dst) != len(tt.src) {
+				t.Fatalf("WriteTo write size=%d, want=%d", len(dst), len(tt.src))
+			}
+			for i := range dst {
+				if dst[i] != tt.src[i] {
+					t.Fatalf("WriteTo result[%d]=%x, want=%x", i, dst[i], tt.src[i])
+				}
+			}
+		}
+	}
+}
+
+func Test_endCentralDirectory(t *testing.T) {
+	src := hexToBytes(`
+        50 4B 05 06 00 00 00 00 0a 00 0a 00 78 56 00 00
+		20 10 00 00 07 00 63 6F 6D 6D 65 6E 74
+	`)
+	expect := endCentralDirectory{
+		numberOfEntries:          10,
+		sizeOfCentralDirectories: 0x5678,
+		offsetCentralDirectory:   0x1020,
+		Comment:                  "comment",
+	}
+
+	{
+		r := bytes.NewReader(src)
+
+		h := new(endCentralDirectory)
+		if _, err := h.ReadFrom(r); err != nil {
+			t.Fatal(err)
+		}
+
+		if h.numberOfEntries != expect.numberOfEntries {
+			t.Errorf("numberOfEntries=%d, want=%d", h.numberOfEntries, expect.numberOfEntries)
+		}
+		if h.sizeOfCentralDirectories != expect.sizeOfCentralDirectories {
+			t.Errorf("sizeOfCentralDirectories=%d, want=%d", h.sizeOfCentralDirectories, expect.sizeOfCentralDirectories)
+		}
+		if h.offsetCentralDirectory != expect.offsetCentralDirectory {
+			t.Errorf("offsetCentralDirectory=%d, want=%d", h.offsetCentralDirectory, expect.offsetCentralDirectory)
+		}
+		if h.Comment != expect.Comment {
+			t.Errorf("Comment=%q, want=%q", h.Comment, expect.Comment)
+		}
+	}
+
+	{
+		w := new(bytes.Buffer)
+		if _, err := expect.WriteTo(w); err != nil {
+			t.Fatal(err)
+		}
+		dst := w.Bytes()
+
+		if len(dst) != len(src) {
+			t.Fatalf("WriteTo write size=%d, want=%d", len(dst), len(src))
+		}
+		for i := range dst {
+			if dst[i] != src[i] {
+				t.Fatalf("WriteTo result[%d]=%x, want=%x", i, dst[i], src[i])
+			}
+		}
+	}
+}
